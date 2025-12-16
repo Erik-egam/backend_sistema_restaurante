c812171f6a96a68828993759be9861d7c7ae7e51
ErikDev <158068793+Erik-egam@users.noreply.github.com>
2025-12-16 18:36:16 -0500

    autenticacion con jwt implementada

--- config/constants.py
+++ config/constants.py
@@ -1,3 +1,8 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+SECRET=os.getenv("SECRET")
+ACCESS_TOKEN_DURATION=int(os.getenv("ACCESS_TOKEN_DURATION"))
+ALGORITHM=os.getenv("ALGORITHM")
--- routes/jwt_auth_users.py
+++ routes/jwt_auth_users.py
@@ -1,26 +1,45 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+from datetime import datetime, timedelta
+
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+
 from models.usuario import Usuario, UsuarioDb
 
 from logic.logica_usuario import LogicaUsuario
 
+from config.constants import ALGORITHM, ACCESS_TOKEN_DURATION, SECRET
 
-router = APIRouter( tags=['Autenticacion basics'] )
+router = APIRouter( tags=[ 'Autenticacion' ] )
 
-oauth2 = OAuth2PasswordBearer(tokenUrl='login')
+oauth2 = OAuth2PasswordBearer( tokenUrl='login' )
 
+crypt = CryptContext( schemes=["bcrypt"] )
 
-async def current_user( token: str = Depends(oauth2) ):
-    usuario: Usuario = LogicaUsuario.buscar_usuario( token )
-    
-    if not usuario:
-        raise HTTPException(
+exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Credenciales invalidas',
             headers={'WWW-Authenticate': "bearer"}
         )
+
+async def usuario_autenticado( token: str = Depends( oauth2 )):
+    try:
+        cedula = jwt.decode( token, SECRET, algorithms= ALGORITHM ).get('sub')
+        if cedula is None:
+            raise exception
         
+        
+
+    except JWTError as e:
+        raise exception
+    
+    return LogicaUsuario.buscar_usuario( cedula )
+
+
+async def current_user( usuario: Usuario = Depends( usuario_autenticado ) ):
+    
     if not usuario.activo:
         raise HTTPException(
             detail="Usuario está inactivo",
@@ -43,19 +62,29 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         detail="usuario no encontrado",
         status_code=status.HTTP_400_BAD_REQUEST
     )
+    
+    
 
-    if not form.password == usuario.password:
+    if not crypt.verify( form.password , usuario.password ):
         raise HTTPException(
             detail='Contraseña incorrecta',
             status_code=status.HTTP_400_BAD_REQUEST
         )
+        
         
     if not usuario.activo:
         raise HTTPException(
             detail="Usuario está inactivo",
             status_code=status.HTTP_400_BAD_REQUEST
         )
+        
+        
+    access_token = {
+        "sub": usuario.cedula,
+        "exp": datetime.utcnow() + timedelta( minutes=ACCESS_TOKEN_DURATION, )
+    }
 
     return {
-        "access_token": usuario.cedula, "token_type": "bearer"
+        "access_token": jwt.encode( access_token, SECRET, algorithm=ALGORITHM ), "token_type": "bearer",
+        "rol": usuario.rol
     }
