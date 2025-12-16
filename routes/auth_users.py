16f9918a15d0ba0ad0538d689f25e666a33e6e9a
ErikDev <158068793+Erik-egam@users.noreply.github.com>
2025-12-16 16:14:28 -0500

    autenticacion basica

--- routes/auth_users.py
+++ routes/auth_users.py
@@ -1,3 +1,61 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-router = FastAPI()
+from models.usuario import Usuario, UsuarioDb
+
+from logic.logica_usuario import LogicaUsuario
+
+
+router = APIRouter( tags=['Autenticacion'] )
+
+oauth2 = OAuth2PasswordBearer(tokenUrl='login')
+
+
+async def current_user( token: str = Depends(oauth2) ):
+    usuario: Usuario = LogicaUsuario.buscar_usuario( token )
+    
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Credenciales invalidas',
+            headers={'WWW-Authenticate': "bearer"}
+        )
+        
+    if not usuario.activo:
+        raise HTTPException(
+            detail="Usuario está inactivo",
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+        
+    return usuario
+
+
+@router.get('/users/me')
+async def perfil( usuario: Usuario = Depends( current_user ) ):
+    return usuario
+
+
+@router.post('/login')
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    usuario: UsuarioDb = LogicaUsuario.buscar_usuario_db(form.username)
+    
+    if not usuario: raise HTTPException(
+        detail="usuario no encontrado",
+        status_code=status.HTTP_400_BAD_REQUEST
+    )
+
+    if not form.password == usuario.password:
+        raise HTTPException(
+            detail='Contraseña incorrecta',
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+        
+    if not usuario.activo:
+        raise HTTPException(
+            detail="Usuario está inactivo",
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    return {
+        "access_token": usuario.cedula, "token_type": "bearer"
+    }
